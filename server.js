6a5ca7b72034a55e731fa294f97d7ad85410873c
sundownspecial <je.mendoza@live.com>
2018-01-03 19:53:56 -0800

    console logged some stuff

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/api/v1/accounts', (req, res) => {
 
 app.get(`/api/v1/verify`, (req, res) => {
   console.log(req)
-  client.query(`SELECT name FROM accounts WHERE name=${req.params.name};`)
+  client.query(`SELECT name FROM accounts WHERE name=${req.query.name};`)
   .then( result => {
     console.log(result.rows)
     return res.send(result.rows)
