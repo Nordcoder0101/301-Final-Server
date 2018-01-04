f6382220b384b6b8d176bb5bdf20c5d724fc403b
Your Name <nords3x4@gmail.com>
2018-01-03 17:43:51 -0800

    update database CRUDS

--- server.js
+++ server.js
@@ -11,40 +11,31 @@ const superAgent = require('superagent');
 const app = express();  
 const PORT = process.env.PORT;
 //this next line of coded added from scots lecture example
-// const DATABASE_URL = process.env.DATABASE_URL;
 const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 //DATABASE setup.
-const client = new pg.Client(DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
+// const client = new pg.Client(DATABASE_URL);
+// client.connect();
+// client.on('error', err => console.error(err));
 
 app.use(cors());
 
 app.get('/api/v1/weather', (req, res) => {
-  console.log(req.query)
-  superAgent.get(`https://api.darksky.net/forecast/68bbafb8dd37fc79325853a950cf330b/${req.query.a},${req.query.b}`)
-    .then( data => {return res.send(data)})
+  superAgent.get(`https://api.darksky.net/forecast/68bbafb8dd37fc79325853a950cf330b/-47.6062,122.3321`)
+    .then( data => console.log(data))
 });
 
 
-//this does nothing for us
-// app.get('/api/v1/location', (req, res) => {
-//   superAgent.get(`http://maps.googleapis.com/maps/api/geocode/json?address=98125`)
-//   .then(results => console.log(results))  
-//   .then( results => {return res.send(results)})
-// });
+app.get('/accounts', (req, res) => {
+  client.query(`SELECT id, name, email, zip;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
 
-
-// app.get('/accounts', (req, res) => {
-//   client.query(`SELECT id, name, email, zip;`)
-//     .then(results => res.send(results.rows))
-//     .catch(console.error);
-// });
-
-app.get('/api/v1/accounts', (req, res) => {
-  client.query(`SELECT * FROM accounts;`)
+app.get('/api/v1/books/:id', (req, res) => {
+  console.log(req.params.id);
+  client.query(`SELECT * FROM books WHERE book_id=$1;`, [req.params.id])
     .then(result => {
       console.log(result.rows);
       return res.send(result.rows);
@@ -53,11 +44,15 @@ app.get('/api/v1/accounts', (req, res) => {
     .catch(console.error);
 });
 
-// app.post('/api/v1/books/', (req) => {
-//   client.query(`INSERT INTO books (title, author, image_url, isbn) VALUES ($!, $2, $3, $4);`, [req.params.title, req.params.author, req.params.image_url, req.params.isbn]);
-// });
+app.post('/api/v1/newaccount/', (req) => {
+  client.query(`INSERT INTO accounts (name, zip, email, password) VALUES ($!, $2, $3, $4);`, [req.params.name, req.params.zip, req.params.email, req.params.password]);
+});
 
-  
+app.get(`/api/v1/verify/`, (req) => {
+  client.query(`SELECT name FROM accounts WHERE name=${name}`)
+  return res.send(result.rows)
+  .catch(console.error);
+}
 
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
